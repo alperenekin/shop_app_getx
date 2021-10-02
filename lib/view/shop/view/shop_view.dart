704fa0ad02e29ddf8a346ff9aba6cd5b6cc000db
alpekin <alperenekin97@gmail.com>
2021-10-02 19:04:50 +0300

    refactor

--- lib/view/shop/view/shop_view.dart
+++ lib/view/shop/view/shop_view.dart
@@ -13,89 +13,114 @@ class ShopView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Shop App'),
       ),
-      body: Obx(() {
-        return shopController.isLoading.value ? Center(
-            child: CircularProgressIndicator()) : GridView.count(
-          childAspectRatio: (.50),
-          crossAxisCount: 2,
-          children: List.generate(
-              shopController.productList?.length ?? 0, (index) {
-            return Card(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding: context.paddingLowHorizontal,
-                    child: Text(shopController.productList?[index]?.title ??
-                        'title', style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),),
-                  ),
-                  Container(
-                    width: context.width * 0.3,
-                    height: context.height * 0.2,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            shopController.productList?[index]?.image ??
-                                'image'),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                  Chip(
-                    label: Container(
-                      width: context.width * 0.15,
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.star_border,
-                            color: Colors.white,
-                          ),
-                          Text(shopController
-                              .productList?[index]?.rating?.rate
-                              .toString() ?? 'count'),
-                        ],
-                      ),
-                    ),
-                    backgroundColor: Colors.blue,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        '€ ${shopController.productList?[index]?.price
-                            .toString() ?? 'price'}',
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .headline5,
-                      ),
-                      Obx(() {
-                        return IconButton(
-                                icon: shopController
-                                        .productList![index]!.isFavourite.value
-                                    ? Icon(
-                                        Icons.favorite,
-                                        color: Colors.red,
-                                      )
-                                    : Icon(
-                                        Icons.favorite_border,
-                                        color: Colors.red,
-                                      ),
-                                onPressed: () {
-                            shopController.productList?[index]
-                                ?.changeFavourite();
-                          },
-                        );
-                      })
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }),
-        );
+      body: buildObxBody(context),
+    );
+  }
+
+  Obx buildObxBody(BuildContext context) {
+    return Obx(() {
+      return shopController.isLoading.value
+          ? Center(child: CircularProgressIndicator())
+          : buildShopGridView(context);
+    });
+  }
+
+  GridView buildShopGridView(BuildContext context) {
+    return GridView.count(
+      childAspectRatio: (.50),
+      crossAxisCount: 2,
+      children: List.generate(shopController.productList?.length ?? 0, (index) {
+        return productCard(context, index);
       }),
+    );
+  }
+
+  Card productCard(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          productTitle(context, index),
+          productImage(context, index),
+          ratingChip(context, index),
+          priceRow(index, context),
+        ],
+      ),
+    );
+  }
+
+  Padding productTitle(BuildContext context, int index) {
+    return Padding(
+      padding: context.paddingLowHorizontal,
+      child: Text(
+        shopController.productList?[index]?.title ?? 'title',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+    );
+  }
+
+  Container productImage(BuildContext context, int index) {
+    return Container(
+      width: context.width * 0.3,
+      height: context.height * 0.2,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              shopController.productList?[index]?.image ?? 'image'),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Chip ratingChip(BuildContext context, int index) {
+    return Chip(
+      label: Container(
+        width: context.width * 0.15,
+        child: Row(
+          children: [
+            Icon(
+              Icons.star_border,
+              color: Colors.white,
+            ),
+            Text(shopController.productList?[index]?.rating?.rate.toString() ??
+                'count'),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.blue,
+    );
+  }
+
+  Row priceRow(int index, BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          '€ ${shopController.productList?[index]?.price.toString() ?? 'price'}',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        Obx(() {
+          return favouriteButton(index);
+        })
+      ],
+    );
+  }
+
+  IconButton favouriteButton(int index) {
+    return IconButton(
+      icon: shopController.productList![index]!.isFavourite.value
+          ? Icon(
+              Icons.favorite,
+              color: Colors.red,
+            )
+          : Icon(
+              Icons.favorite_border,
+              color: Colors.red,
+            ),
+      onPressed: () {
+        shopController.productList?[index]?.changeFavourite();
+      },
     );
   }
 }
